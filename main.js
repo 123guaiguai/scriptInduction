79fd66334d65f39171363d226ae0871669526072
zuoshaodao <1952664675@qq.com>
2022-11-14 23:42:08 +0800

    commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // @name         dangkezhushou
 // @name:zh-CN   党课自动播放脚本         
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  可以自动点击暂停弹窗，自动播放党课
 // @author       123guaiguai
 // @match        https://dxpx.uestc.edu.cn/jjfz/*
@@ -13,10 +13,10 @@
 
 (function () {
     'use strict';
-    let lesson_li=document.querySelector(".w1150").querySelector(".lesson_center_ul").querySelectorAll("li");
+    let lesson_li=document.querySelector(".w1150").querySelector(".lesson_center_ul").getElementsByTagName("li");
     console.log(lesson_li);
     let lesson_btn=lesson_li.map((item)=>{
-        return item.querySelector(".lesson_center_a").querySelectorAll("a")[0];
+        return item.querySelector(".lesson_center_a").getElementsByTagName("a")[0];
     })
     console.log(lesson_btn);
     // Your code here...
