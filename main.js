ccddf0b680035855e753c4920c56ca01069be62c
zuoshaodao <1952664675@qq.com>
2022-11-15 00:12:16 +0800

    commit

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 // @name         dangkezhushou
 // @name:zh-CN   党课自动播放脚本         
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  可以自动点击暂停弹窗，自动播放党课
 // @author       123guaiguai
 // @match        https://dxpx.uestc.edu.cn/jjfz/*
@@ -14,10 +14,10 @@
 (function () {
     'use strict';
     let lesson_li=document.querySelector(".wrap .w1150 .lesson_center_ul .lesson_c_ul").querySelectorAll("li");
-    console.log(lesson_li);
-    // let lesson_btn=lesson_li.map((item)=>{
-    //     return item.querySelector(".lesson_center_a").getElementsByTagName("a")[0];
-    // })
-    // console.log(lesson_btn);
-    // Your code here...
+    //console.log(lesson_li);
+    let lesson_btn=[];
+    for(let i=0;i<lesson_li.length;i++){
+        lesson_btn.push(lesson_li[i].querySelector(".lesson_center_a").firstChild);
+    }
+    console.log(lesson_btn);
 })();
