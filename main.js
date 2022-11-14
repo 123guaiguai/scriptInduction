67ee3a1fd251763b4013c0552f80fbd990cdf9bd
zuoshaodao <1952664675@qq.com>
2022-11-14 23:38:58 +0800

    commit

--- main.js
+++ main.js
@@ -2,18 +2,22 @@
 // @name         dangkezhushou
 // @name:zh-CN   党课自动播放脚本         
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  可以自动点击暂停弹窗，自动播放党课
 // @author       123guaiguai
-// @match        https://dxpx.uestc.edu.cn/jjfz/lesson/*
-// @match        https://dxpx.uestc.edu.cn/jjfz/lesson
+// @match        https://dxpx.uestc.edu.cn/jjfz/*
 // @license      AGPL-3.0
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-    console.log("hello,world!")
+    let lesson_li=document.querySelector(".w1150").querySelector(".lesson_center_ul").querySelectorAll("li");
+    console.log(lesson_li);
+    let lesson_btn=lesson_li.map((item)=>{
+        return item.querySelector(".lesson_center_a").querySelectorAll("a")[0];
+    })
+    console.log(lesson_btn);
     // Your code here...
 })();
