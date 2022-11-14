130b913c3c0206dd4e0999d070a9d79e28f1d359
zuoshaodao <1952664675@qq.com>
2022-11-14 18:22:38 +0800

    second commit

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
 // ==UserScript==
+// @name         dangkezhushou
 // @name:zh-CN   党课自动播放脚本         
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  可以自动点击暂停弹窗，自动播放党课
 // @author       123guaiguai
 // @match        https://dxpx.uestc.edu.cn/jjfz/lesson/*
+// @match        https://dxpx.uestc.edu.cn/jjfz/lesson
+// @license      AGPL-3.0
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-    setTimeout(()=>{
-        window.location.href="https://www.bilibili.com/";
-        setTimeout(()=>{
-            window.location.href="https://github.com/";
-        },2000)
-    },2000)
+    console.log("hello,world!")
     // Your code here...
 })();
